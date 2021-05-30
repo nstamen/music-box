30a2d1a147436d062d08aa426e9299dd55611e47
nstamen <85070216+nstamen@users.noreply.github.com>
2021-05-30 11:15:26 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+})
